27af2cee0d5c4fbf3cb7e366e9664b1b5af807b0
Felthrup <worten@gmail.com>
2016-11-16 11:45:42 +0100

    Add files via upload

---
+++ Club.java
@@ -0,0 +1,16 @@
+public class Club
+{
+   private static String name = "Delfinen"; // klub navn
+   private static Person<Member> members; // arrayliste over medlemmer
+   
+   // forventer ikke understående bliver relevante i programmet
+   // cashier
+   // chairman
+   // trainers
+   
+   
+   public static void addMember(Member newMember)
+   {
+      // members.add(newMember);
+   }
+}
